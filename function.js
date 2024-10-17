0ecc0e5cef18620a228e75361c8c669dd519416c
hutter <hutter@SWSNBLUHU2022>
2024-10-17 09:07:47 +0200

    ChangeButton

--- function.js
+++ function.js
@@ -1,11 +1,10 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementsByClassName('sidebar')[0].style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+function changeNav() {
+  var sidebarElements = document.getElementById("sidebarelements")
+  if (sidebarElements.style.display === "none"){
+    sidebarElements.style.display = "block";
+  } else {
+    sidebarElements.style.display = "none";
   }
-  
+}
   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-  function closeNav() {
-    document.getElementsByClassName('sidebar')[0].style.width = "30px";
-    document.getElementById("main").style.marginLeft = "30px";
-  }
