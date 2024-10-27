081d1331294734af433440077d12fd518ad699b8
JonathanForker <jonathan-forker@web.de>
2024-10-27 02:50:23 +0200

    PHP und Login

---
+++ loginfunctions.js
@@ -0,0 +1,12 @@
+function togglePassword() {
+    const passwordInput = document.getElementById("password");
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+    } else {
+        passwordInput.type = "password";
+    }
+}
+
+function showAlert(message) {
+    alert(message);
+}
