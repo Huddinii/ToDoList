aa334011151e4f409127db0e5eb4b159ef7347d9
JonathanForker <jonathan-forker@web.de>
2024-11-09 01:44:19 +0100

    Drag and Drop

--- function.js
+++ function.js
@@ -8,3 +8,65 @@ function changeNav() {
   }
 }
   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+
+const items = document.querySelectorAll(".PriorityCategories .item");
+const lists = document.querySelectorAll(".PriorityCategories .PriorityElement");
+let currentItem = null;
+
+function dragStart(e){
+  if(![...e.target.classList].includes("item")){
+    e.preventDefault();
+    return;
+  }
+  currentItem = e.target;
+  e.target.classList.add("drag-active");
+  setTimeout(()=>{
+    e.target.style.display = "none";
+  },0);
+}
+
+  function dragEnd(e) {
+    currentItem = null;
+    e.target.classList.remove("drag-active")
+    e.target.style.display = "flex";
+  }
+  
+  function dragOver(e) {
+    e.preventDefault();
+  }
+
+  function dragEnter(e) {
+    e.preventDefault();
+    e.target.classList.add("hoverd");
+  }
+
+  function dragLeave(e) {
+    e.preventDefault();
+    e.target.classList.remove("hoverd");
+  }
+
+function drop(e) {
+  if(![...e.target.classList].includes("PriorityElement")){
+    e.preventDefault();
+    return;
+  }
+  e.target.classList.remove("hoverd");
+  e.target.append(currentItem);
+  currentItem = null;
+}
+
+items.forEach((item) => {
+  item.addEventListener("dragstart", dragStart);
+  item.addEventListener("dragend", dragEnd);
+});
+
+lists.forEach((PriorityElement) => {
+  PriorityElement.addEventListener("dragover", dragOver);
+  PriorityElement.addEventListener("dragenter", dragEnter);
+  PriorityElement.addEventListener("dragleave", dragLeave);
+  PriorityElement.addEventListener("drop", drop);
+
+})
+
+
+
