1a30ddf4b15d3a922ef5d6f4e1f86a188f64270d
JonathanForker <133353421+JonathanForker@users.noreply.github.com>
2024-12-13 15:15:22 +0000

    Design

--- login.php
+++ login.php
@@ -9,16 +9,16 @@
     <div class="login-container">
         <h2>Login</h2>
         <form action="login.php" method="POST">
+            <div class="div_text">
+                <p>Noch keinen Accout klick <a href="register.php">hier</a></p>
+            </div> 
             <label for="username">Benutzername</label>
             <input type="text" id="username" name="username" required>
             <label for="password">Passwort</label>
             <input type="password" id="password" name="password" required>
         <div class="checkbox-container">
             <input type="checkbox" id?="show-password" onclick="togglePassword()">Passwort anzeigen </input>
-        </div>
-        <div class="div_text">
-            <p>Noch keinen Accout klick <a href="register.php">hier</a></p>
-        </div>  
+        </div> 
             <button type="submit" value="Einloggen" onclick="login()">Einloggen</button>
     </div>
 </form>
--- register.php
+++ register.php
@@ -9,6 +9,9 @@
     <div class="register-container">
         <h2>Login</h2>
         <form action="register.php" method="POST">
+            <div class="div_text">
+                <p>Du hast einen Accout? klick <a href="login.php">hier</a></p>
+            </div> 
             <label for="username">Benutzername</label>
             <input type="text" id="username" name="username" required>
             <label for="E-Mail">E-Mail</label>
@@ -17,10 +20,7 @@
             <input type="password" id="password" name="password" required>
         <div class="checkbox-container">
             <input type="checkbox" id?="show-password" onclick="togglePassword()">Passwort anzeigen </input>
-        </div>
-        <div class="div_text">
-            <p>Du hast einen Accout? klick <a href="login.php">hier</a></p>
-        </div>  
+        </div> 
             <button type="submit" value="Einloggen" onclick="login()">Registrieren</button>
     </div>
 </form>
